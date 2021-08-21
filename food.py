e6a4bdd926d18c314f2da3c20fcf9697cdef59a0
singhap88 <64716984+singhap88@users.noreply.github.com>
2021-08-21 15:48:06 -0400

    Update food.py

--- food.py
+++ food.py
@@ -1,8 +1,8 @@
 from turtle import Turtle
 import random
 
-# The food in the snake game is the objective the snake must get to and how it accrues points
-# The refresh function picks a random coordinate in the plane for the snake to spawn
+# The food in the snake game is the objective the snake must get to and how it accrues points.
+# The refresh function picks a random coordinate in the plane for the snake to spawn.
 
 class Food(Turtle):
 
